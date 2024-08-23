1d67f9611a41d3c45ad1b1ac712bc0af015f8cfc
appsaprinsky <app1.n.saprinsky@gmail.com>
2024-08-23 18:50:07 +0200

    Update portfolio

--- portfolio.py
+++ portfolio.py
@@ -4,8 +4,32 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 
-tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'META']
-data = yf.download(tickers, start='2020-01-01', end='2024-08-01')['Adj Close']
+DATE_START = '2020-01-01'
+DATE_END = '2024-08-23'
+tickers = [
+    'AAPL',  # Apple Inc.
+    'MSFT',  # Microsoft Corp.
+    'GOOGL', # Alphabet Inc. (Google)
+    'AMZN',  # Amazon.com Inc.
+    'TSLA',  # Tesla Inc.
+    'META',  # Meta Platforms Inc. (Facebook)
+    'NVDA',  # NVIDIA Corporation
+    'JNJ',   # Johnson & Johnson
+    'V',     # Visa Inc.
+    'PG',    # Procter & Gamble Co.
+    'JPM',   # JPMorgan Chase & Co.
+    'UNH',   # UnitedHealth Group Inc.
+    'HD',    # Home Depot Inc.
+    'MA',    # Mastercard Inc.
+    'DIS',   # The Walt Disney Company
+    'NFLX',  # Netflix Inc.
+    'BABA',  # Alibaba Group Holding Ltd.
+    'XOM',   # Exxon Mobil Corporation
+    'BAC',   # Bank of America Corp.
+    'KO',    # The Coca-Cola Company
+    'PEP'    # PepsiCo Inc.
+]
+data = yf.download(tickers, start=DATE_START, end=DATE_END)['Adj Close']
 print(data.head())
 returns = data.pct_change().dropna() # calculate daily returns
 print(returns.head())
@@ -31,6 +55,7 @@ initial_weights = [1/len(tickers)] * len(tickers)# initial guess (equal weightin
 ############# Optimize the portfolio #############
 optimized = minimize(sharpe_ratio, initial_weights, args=(returns,), method='SLSQP', bounds=bounds, constraints=constraints)
 optimized_weights = optimized.x
+print(f"Shares: {data.columns}")
 print(f"Optimized Weights: {optimized_weights}")
 portfolio_returns = returns.dot(optimized_weights)
 cumulative_returns = (1 + portfolio_returns).cumprod()
@@ -48,3 +73,20 @@ optimized_sharpe_ratio = optimized_return / optimized_volatility
 print(f"Optimized Portfolio Return: {optimized_return:.2%}")
 print(f"Optimized Portfolio Volatility: {optimized_volatility:.2%}")
 print(f"Optimized Sharpe Ratio: {optimized_sharpe_ratio:.2f}")
+
+
+''' OUTPUT
+Shares: Index(['AAPL', 'AMZN', 'BABA', 'BAC', 'DIS', 'GOOGL', 'HD', 'JNJ', 'JPM', 'KO',
+       'MA', 'META', 'MSFT', 'NFLX', 'NVDA', 'PEP', 'PG', 'TSLA', 'UNH', 'V',
+       'XOM'],
+      dtype='object', name='Ticker')
+Optimized Weights: [2.46056003e-17 0.00000000e+00 2.54785000e-16 0.00000000e+00
+ 0.00000000e+00 0.00000000e+00 1.52857093e-16 1.88556352e-16
+ 2.23407263e-16 9.78095214e-17 0.00000000e+00 6.76893180e-17
+ 0.00000000e+00 9.57046358e-17 5.23611926e-01 8.48722741e-17
+ 8.23132679e-02 1.10725656e-01 9.55664883e-02 1.37156800e-17
+ 1.87782662e-01]
+Optimized Portfolio Return: 56.28%
+Optimized Portfolio Volatility: 36.46%
+Optimized Sharpe Ratio: 1.54
+'''
